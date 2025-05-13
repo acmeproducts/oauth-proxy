f4da622edfc3562ebed4b0171e9e7a37c8324ff3
acmeproducts <myacctfortracking@gmail.com>
2025-05-13 04:57:03 -0700

    Update multi_app_proxy_github.js

--- multi_app_proxy_github.js
+++ multi_app_proxy_github.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 // Config
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const GITHUB_PAT = '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';
+const GITHUB_PAT = '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';
 const GITHUB_REPO = 'acmeproducts/oauth-proxy';
 const GITHUB_FILE = 'tokens.json';
 
